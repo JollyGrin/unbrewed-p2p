45460ae71107c7615fe2372912ec335cee1fa4cb
jollygrin <lol@dean.lol>
2023-04-18 01:13:22 +0200

    test usequey

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/styles/style";
+import { useState } from "react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
-      <DocumentHeader />
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <DocumentHeader />
+          <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
--- pages/p2p/index.tsx
+++ pages/p2p/index.tsx
@@ -1,19 +1,34 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Spinner, Text } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { FaBeer, FaAddressBook } from "react-icons/fa";
+import axios from "axios";
 
 const P2PContainer = () => {
   const router = useRouter();
   const slug = router.query;
-  console.log({ slug });
 
   const [bool, setBool] = useState(false);
+
+  const { data, isLoading } = useQuery(["myData"], async () => {
+    try {
+      const result = await axios.get("	https://get.geojs.io/v1/ip/country.json");
+      console.log({ result });
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  console.log({ data });
   return (
     <Box>
       <Text>{JSON.stringify(slug)}</Text>
       <Button onClick={() => setBool(!bool)}>Bool</Button>
       {bool ? <FaBeer /> : <FaAddressBook />}
+      {isLoading && <Spinner />}
+      {data && <Text>{JSON.stringify(data.data)}</Text>}
     </Box>
   );
 };
