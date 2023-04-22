44b534e17f297a93c9f1e9ad095def2641979423
jollygrin <lol@dean.lol>
2023-04-22 14:57:22 +0200

    test font fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const pathPrefix = process.env.NODE_ENV === "production" ? "/unbrewed-v2" : "";
 
-console.log("xxxx", process.env.NODE_ENV);
+console.log("Environment:", process.env.NODE_ENV);
 const nextConfig = {
   assetPrefix: pathPrefix,
   env: {
