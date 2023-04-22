75a9e4c50cd55c69005a3553a35e9a02738aba59
jollygrin <lol@dean.lol>
2023-04-22 20:59:47 +0200

    backpack

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import "@/styles/fonts.css";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -21,6 +22,22 @@ export default function App({ Component, pageProps }: AppProps) {
         <Hydrate state={pageProps.dehydratedState}>
           <DocumentHeader />
           <ChakraProvider theme={theme}>
+            <Toaster
+              position="top-center"
+              reverseOrder={false}
+              gutter={8}
+              containerClassName=""
+              containerStyle={{}}
+              toastOptions={{
+                // Define default options
+                className: "",
+                duration: 5000,
+                style: {
+                  background: "#363636",
+                  color: "#fff",
+                },
+              }}
+            />
             <Component {...pageProps} />
           </ChakraProvider>
         </Hydrate>
--- pages/backpack.tsx
+++ pages/backpack.tsx
@@ -1,34 +1,66 @@
+//@ts-nocheck
 import { useQuery } from "@tanstack/react-query";
-import { Box, Button, Flex, Spinner, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Spinner,
+  Tag,
+  TagLabel,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { useState } from "react";
 import { CardFactory } from "@/components/CardFactory/card.factory";
-import { DeckImportDataType } from "@/components/DeckPool/deck-import.type";
+import { useDebounce } from "use-debounce";
+import { toast } from "react-hot-toast";
+import { DECK_ID } from "@/lib/constants/unmatched-deckids";
 
 const BackpackPage = () => {
   const [deckId, setDeckId] = useState<string>();
+  const [deckIdDebounced] = useDebounce(deckId, 300);
   const [apiUrl, setApiUrl] = useState<string>(
     "https://unbrewed-api.vercel.app/api/unmatched-deck/"
   );
   const [selectedCardIndex, setSelectedCardIndex] = useState<number>(0);
 
-  const { data, isLoading } = useQuery(
-    ["deck"],
+  const { data, isLoading, error } = useQuery(
+    ["deck", deckIdDebounced, setApiUrl],
     async () => {
       try {
-        const result = await axios.get(apiUrl + deckId);
+        const result = await axios.get(apiUrl + deckIdDebounced);
         return result.data;
       } catch (err) {
         console.error(err);
       }
     },
-    { enabled: !!deckId }
+    {
+      enabled: !!deckIdDebounced,
+      onSuccess: (e) => toast.success("Deck fetched!"),
+      onError: (e) => toast.error("Error fetching deck"),
+    }
   );
 
   return (
-    <Box>
-      <Text>Load a deck</Text>
-      <Button onClick={() => setDeckId("pk1x")}>Load Thrall</Button>
+    <Box p={3}>
+      <Text p={"1rem 0"}>
+        <a href="https://unmatched.cards/decks" target="_blank">
+          <Tag>Find</Tag>
+        </a>{" "}
+        and Load a deck
+      </Text>
+      <Flex gap={2} flexDir={"column"}>
+        <Input
+          w="50%"
+          placeholder={DECK_ID.THRALL}
+          onChange={(e) => setDeckId(e.target.value)}
+        />
+        <Tag fontFamily={"monospace"} fontSize={"0.75rem"} opacity={"0.5"}>
+          {apiUrl + deckIdDebounced}
+        </Tag>
+      </Flex>
       {!!deckId && isLoading && <Spinner />}
 
       {data?.deck_data && (
@@ -60,7 +92,33 @@ const BackpackPage = () => {
           <code>{JSON.stringify(data.deck_data.cards[selectedCardIndex])}</code>
         </Box>
       )}
+      {error && <ErrorBox error={error} />}
+
+      <Box pt={3}>
+        <Text>
+          ...or{" "}
+          <a
+            href="https://github.com/JollyGrin/unbrewed-api"
+            style={{ textDecoration: "underline" }}
+          >
+            change the url used to fetch the deck
+          </a>
+        </Text>
+        <Input
+          w="50%"
+          defaultValue={apiUrl}
+          onChange={(e) => setApiUrl(e.target.value)}
+        />
+      </Box>
     </Box>
   );
 };
 export default BackpackPage;
+
+const ErrorBox = ({ error }) => {
+  return (
+    <Box minH={"200px"} p={3} borderRadius={5} bg="gray">
+      <Text color="tomato">{JSON.stringify(error)}</Text>
+    </Box>
+  );
+};
