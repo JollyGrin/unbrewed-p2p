a8015eb1de270e5dde34447dcbbbc30455aa59a4
jollygrin <JollyGrin@protonmail.com>
2024-03-17 01:47:00 +0100

    add maps

--- components/Bag/Map/index.tsx
+++ components/Bag/Map/index.tsx
@@ -4,6 +4,9 @@ import { useState } from "react";
 import { AddNewFields } from "./AddNewMapFields";
 import { LinkIcon } from "@chakra-ui/icons";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { MapModal } from "./MapModal";
+import { DEFAULT_MAPS } from "./MapModal/defaultMaps";
 
 export const BagMap = () => {
   const { data, clear } = useLocalMapStorage();
@@ -22,37 +25,58 @@ export const BagMap = () => {
         <AddNewFields {...{ newMap, enterMapUrl, setNewMap }} />
         <Image alt="map-preview" src={newMap?.imgUrl} />
       </Grid>
-      {data?.map((map) => {
-        return (
-          <Flex
-            maxW="500px"
-            m="1rem auto"
-            bg="brand.secondary"
-            color="brand.primary"
-            justifyContent="space-between"
-            alignItems="center"
-            borderRadius="0.5rem"
-          >
-            <HStack p="1rem">
-              <Text fontSize="2rem">{map.meta?.title}</Text>
-              {map.meta?.author && <Text>by {map.meta.author}</Text>}
-              {map.meta?.url && (
-                <Link href={map.meta.url}>
-                  <LinkIcon fontSize="2rem" />
-                </Link>
-              )}
-            </HStack>
-            <Box
-              boxSize="10rem"
-              bgImage={map.imgUrl}
-              bgPosition="center"
-              bgSize="cover"
-              borderRightRadius="inherit"
-            />
-          </Flex>
-        );
-      })}
-      <Button onClick={clear}>Clear all Maps</Button>
+      {data?.map((map) => <MapCard key={map.imgUrl} {...map} />)}
+      <Button onClick={clear} ml="1rem">
+        Clear your saved Maps
+      </Button>
+
+      <Text
+        m="0 auto"
+        w="fit-content"
+        fontSize="2rem"
+        fontFamily="SpaceGrotesk"
+      >
+        Default Homebrewed Maps
+      </Text>
+      {DEFAULT_MAPS?.map((map) => <MapCard key={map.imgUrl} {...map} />)}
+
+      <MapModal />
     </Box>
+  );
+};
+
+const MapCard = (map: MapData) => {
+  const { query, push } = useRouter();
+  return (
+    <Flex
+      maxW="500px"
+      m="1rem auto"
+      bg="brand.secondary"
+      color="brand.primary"
+      justifyContent="space-between"
+      alignItems="center"
+      borderRadius="0.5rem"
+    >
+      <HStack p="1rem">
+        <Text fontSize="2rem">{map.meta?.title}</Text>
+        {map.meta?.author && <Text>by {map.meta.author}</Text>}
+        {map.meta?.url && (
+          <Link href={map.meta.url}>
+            <LinkIcon fontSize="2rem" />
+          </Link>
+        )}
+      </HStack>
+      <Box
+        cursor="pointer"
+        boxSize="10rem"
+        bgImage={map.imgUrl}
+        bgPosition="center"
+        bgSize="cover"
+        borderRightRadius="inherit"
+        onClick={() => {
+          push({ query: { ...query, mapUrl: map.imgUrl, editMapUrl: true } });
+        }}
+      />
+    </Flex>
   );
 };
