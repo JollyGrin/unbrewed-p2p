b6042eaf8a8d47f0231bf15e9a428ac8453f8fd9
jollygrin <JollyGrin@protonmail.com>
2024-03-26 22:26:30 +0100

    sharable deck link

--- components/Connect/index.tsx
+++ components/Connect/index.tsx
@@ -2,15 +2,18 @@ import {
   Box,
   Button,
   Flex,
+  FormLabel,
+  Grid,
   HStack,
   Input,
+  Select,
   Spinner,
   Tag,
   Text,
   VStack,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   useLocalDeckStorage,
   useLocalServerStorage,
@@ -21,16 +24,21 @@ import { Navbar } from "@/components/Navbar";
 import { SelectedDeckContainer } from "./SelectedDeck";
 import { useCreateLobby } from "./useCreateLobby";
 import styled from "@emotion/styled";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { useCopyToClipboard } from "@/lib/hooks/useCopyToClipboard";
+import { toast } from "react-hot-toast";
 
 export const ConnectPage = () => {
   const router = useRouter();
   const nameRef = useRef<HTMLInputElement>(null);
   const gidRef = useRef<HTMLInputElement>(null);
 
+  const [sharedDeckId, setSharedDeckId] = useState<string | undefined>();
+
   // uses router props to load new decks
   useLoadRouterDeck();
 
-  const { starredDeck } = useLocalDeckStorage();
+  const { starredDeck, decks } = useLocalDeckStorage();
   const { activeServer, setActiveServer, serverList } = useLocalServerStorage();
 
   // create the lobby and connect to it
@@ -38,6 +46,8 @@ export const ConnectPage = () => {
     gidRef,
     nameRef,
   });
+
+  const [_, copy] = useCopyToClipboard();
 
   useEffect(() => {
     router.prefetch("/game");
@@ -54,12 +64,17 @@ export const ConnectPage = () => {
         <Navbar />
       </Box>
       <ConnectContainer backdropFilter="blur(5px)">
-        <Text fontSize={"2.5rem"} fontFamily="SpaceGrotesk">
-          Connect
+        <Text fontSize={"2.5rem"} fontWeight={700} fontFamily="SpaceGrotesk">
+          Connect to a Lobby
         </Text>
         <SelectedDeckContainer />
-        <VStack m={"2rem auto"}>
-          <HStack>
+        <Grid
+          templateColumns={`1fr ${sharedDeckId === undefined ? "auto" : ""} 1fr`}
+          w="100%"
+          gap="0.5rem"
+        >
+          {sharedDeckId === undefined && <div />}
+          <VStack>
             <Input
               ref={nameRef}
               defaultValue={router.query.username as string | undefined}
@@ -69,28 +84,64 @@ export const ConnectPage = () => {
             <Input
               ref={gidRef}
               defaultValue={router.query.lobby as string | undefined}
-              placeholder="room name"
+              placeholder="lobby name"
               bg="white"
             />
-          </HStack>
-          <Button
-            // If we are loading a new lobby request, freeze the input values.
-            // isDisabled={(data && isLoading) || starredDeck === undefined}
-            isDisabled={loading || starredDeck === undefined}
-            bg="brand.primary"
-            onClick={() => {
-              if (!nameRef?.current?.value || !gidRef?.current?.value) {
-                alert("I need a name and a room name");
-                return;
-              }
-              setLoading(true);
-              refetch();
-            }}
-          >
-            {loading && <Spinner />}
-            Connect to Game
-          </Button>
-        </VStack>
+            <Button
+              // If we are loading a new lobby request, freeze the input values.
+              // isDisabled={(data && isLoading) || starredDeck === undefined}
+              w="100%"
+              isDisabled={loading || starredDeck === undefined}
+              bg="brand.primary"
+              onClick={() => {
+                if (!nameRef?.current?.value || !gidRef?.current?.value) {
+                  alert("I need a name and a room name");
+                  return;
+                }
+                setLoading(true);
+                refetch();
+              }}
+            >
+              {loading && <Spinner />}
+              Connect to Game
+            </Button>
+          </VStack>
+          {sharedDeckId === undefined && (
+            <PlusSquareIcon
+              onClick={() => setSharedDeckId("")}
+              fontSize="2rem"
+              alignSelf="center"
+              justifySelf="center"
+            />
+          )}
+
+          {sharedDeckId !== undefined && (
+            <VStack>
+              <FormLabel mt="7px">Share a link with the deck:</FormLabel>
+              <Select
+                bg="white"
+                onChange={(e) => setSharedDeckId(e.target.value)}
+              >
+                {decks?.map((deck) => (
+                  <option key={deck.id} value={deck.version_id ?? deck.id}>
+                    {deck.name}
+                  </option>
+                ))}
+              </Select>
+              <Button
+                w="100%"
+                onClick={() => {
+                  const baseUrl = "https://unbrewed.xyz/connect";
+                  const sharableUrl = `${baseUrl}?lobby=${gidRef?.current?.value}&deckId=${sharedDeckId}`;
+                  copy(sharableUrl);
+                  toast.success("Copied to clipboard: " + sharableUrl);
+                }}
+              >
+                Copy Sharable Link
+              </Button>
+            </VStack>
+          )}
+        </Grid>
       </ConnectContainer>
       <Flex
         position="absolute"
@@ -122,7 +173,8 @@ const ConnectContainer = styled(Flex)`
   height: 50%;
   width: 95%;
   max-width: 600px;
-  background-color: rgba(255, 255, 255, 0.9);
+  min-height: 600px;
+  background-color: rgba(255, 255, 255, 0.8);
   border-radius: 1rem;
   padding: 2rem;
   justify-content: space-evenly;
