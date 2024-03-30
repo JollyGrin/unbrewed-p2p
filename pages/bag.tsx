11e633b0249d70025607ecf1896fd851c1e936c0
jollygrin <JollyGrin@protonmail.com>
2024-03-30 13:58:34 +0100

    update

--- pages/bag.tsx
+++ pages/bag.tsx
@@ -12,14 +12,24 @@ import { Navbar } from "@/components/Navbar";
 import { BagDecks } from "@/components/Bag/Deck";
 import { BagMap } from "@/components/Bag/Map";
 import { BagBulkContainer } from "@/components/Bag/Bulk";
+import { useRouter } from "next/router";
 
 const BagPage = () => {
+  const { query, push } = useRouter();
+  const tab = query?.tab as number | undefined;
+
   return (
     <Flex flexDir={"column"} bg="brand.highlight" h="100svh">
       <Box color="brand.secondary">
         <Navbar />
       </Box>
-      <Tabs h="100%">
+      <Tabs
+        h="100%"
+        index={tab ? +tab : 0}
+        onChange={(e) => {
+          push({ query: { ...query, tab: e.toString() } });
+        }}
+      >
         <TabList>
           <Tab>Decks</Tab>
           <Tab>Maps</Tab>
