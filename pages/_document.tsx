c3947b5c9ce7e400ebdd898b1740a4c21711851e
jollygrin <JollyGrin@protonmail.com>
2024-03-30 17:21:43 +0100

    remove build error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      {/* @ts-expect-error: jsx */}
       <Head />
       <body>
         <Main />
-        {/* @ts-expect-error: jsx */}
         <NextScript />
       </body>
     </Html>
