bfe3f63e378e731ee7347010b536931da11a10a4
jollygrin <JollyGrin@protonmail.com>
2024-03-30 17:25:17 +0100

    add more ts ignore

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      {/* @ts-ignore: weird github actions issue */}
       <Head />
       <body>
+        {/* @ts-ignore: weird github actions issue */}
         <Main />
+        {/* @ts-ignore: weird github actions issue */}
         <NextScript />
       </body>
     </Html>
