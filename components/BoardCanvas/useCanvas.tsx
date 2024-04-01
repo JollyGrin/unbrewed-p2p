9beba9f2cee9dce61572e351c46518a17ae8db75
jollygrin <JollyGrin@protonmail.com>
2024-04-01 16:00:56 +0200

    working with icon

--- components/BoardCanvas/useCanvas.tsx
+++ components/BoardCanvas/useCanvas.tsx
@@ -55,12 +55,12 @@ export const useCanvas = ({
       .data(data)
       .join((enter) => enter.append("g"))
       .attr("transform", (d) => `translate(${d.x}, ${d.y})`)
+      .attr("fill", ({ color }) => color ?? "black")
       .html(
-        `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" viewBox="0 0 24 24">
   <path fill-rule="evenodd" d="M7 2a2 2 0 0 0-2 2v1a1 1 0 0 0 0 2v1a1 1 0 0 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7Zm3 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm-1 7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3 1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
 </svg>`,
       )
-      .attr("fill", ({ color }) => color ?? "black")
       // TODO: replace this to limit which token the user can control
       .attr("opacity", ({ id }) => (id.includes(self as string) ? 1 : 0.75))
       .filter(({ id }) => {
@@ -70,7 +70,7 @@ export const useCanvas = ({
       })
       .call(
         d3
-          .drag<SVGCircleElement, PositionType>()
+          .drag<SVGGElement, PositionType>()
           .on("start", dragstarted)
           .on("drag", dragged)
           .on("end", dragended)
