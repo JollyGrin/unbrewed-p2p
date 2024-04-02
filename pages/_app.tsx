0520c52e5d9651ceac4b5ffa8e6812fecbf54f7f
jollygrin <JollyGrin@protonmail.com>
2024-04-03 01:29:08 +0200

    remove error boundary - doesnt work with next export

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,40 +12,37 @@ import {
 } from "@tanstack/react-query";
 import "@/styles/fonts.css";
 import { Toaster } from "react-hot-toast";
-import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <>
-      <ErrorBoundary>
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={pageProps.dehydratedState}>
-            <DocumentHeader />
-            <ChakraProvider theme={theme}>
-              <Toaster
-                position="top-center"
-                reverseOrder={false}
-                gutter={8}
-                containerClassName=""
-                containerStyle={{}}
-                toastOptions={{
-                  // Define default options
-                  // className: "unbrewed-",
-                  duration: 5000,
-                  style: {
-                    background: "#363636",
-                    color: "#fff",
-                  },
-                }}
-              />
-              {/* @ts-ignore */}
-              <Component {...pageProps} />
-            </ChakraProvider>
-          </Hydrate>
-        </QueryClientProvider>
-      </ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <DocumentHeader />
+          <ChakraProvider theme={theme}>
+            <Toaster
+              position="top-center"
+              reverseOrder={false}
+              gutter={8}
+              containerClassName=""
+              containerStyle={{}}
+              toastOptions={{
+                // Define default options
+                // className: "unbrewed-",
+                duration: 5000,
+                style: {
+                  background: "#363636",
+                  color: "#fff",
+                },
+              }}
+            />
+            {/* @ts-ignore */}
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
